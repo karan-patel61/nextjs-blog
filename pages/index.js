4a75cad9c5abd1e7a0635043e88afd4d095ac726
karan-patel61 <patel_karan_cool@hotmail.com>
2023-07-24 17:45:41 -0400

    Latest message modified

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ posts }) {
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
             <br/>
-            Extra Text goes here!
+            Check out some of the most recent articles below.
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
