7941e56d9c489d35ccd9bfdd7135627c3088a371
karan-patel61 <patel_karan_cool@hotmail.com>
2023-07-29 12:54:06 -0400

    added OpenChat and FileUpload project details

--- data/projectsData.js
+++ data/projectsData.js
@@ -7,6 +7,19 @@ const projectsData = [
     href: 'https://www.karanpatelcv.com',
   },
   {
+    title: 'Open Chat',
+    description: `A public chat room web application built and deployed using Google Firebase.`,
+    imgSrc: '/static/images/firebase.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'File Upload',
+    description: `A full-stack MERN application serving as an Employee Database.
+    In addition, a file upload feature was implemented to allow uploading images.`,
+    imgSrc: '/static/images/mongodb-logo.png',
+    href: '/blog/the-time-machine',
+  },
+  {
     title: 'The Time Machine',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
